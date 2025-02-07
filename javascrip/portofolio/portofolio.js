29ee2f73c3248742098866322b5ada5cdf591584
dimaswidysaputra41@gamil.com <dimaswidysaputra41@gmail.com>
2025-02-07 21:04:34 +0700

    gak ruh

--- javascrip/portofolio/portofolio.js
+++ javascrip/portofolio/portofolio.js
@@ -5,30 +5,17 @@ const teks = document.querySelector(".teks").cloneNode(true);
 
 document.querySelector(".container-teks").appendChild(teks);
 
-// const text = document.querySelector(".text").cloneNode(true);
-// document.querySelector(".header").appendChild(text);
-
-// const element = document.getElementById("card");
-// const elementTwo = document.getElementById("header");
-// const elementtes = document.querySelector(".animated-element");
-
-// // Tambahkan event listener untuk scroll
-// window.addEventListener("scroll", () => {
-//   // Ambil posisi scroll saat ini
-//   const scrollPosition = window.scrollY;
-
-//   // Tentukan kapan elemen akan bergerak keluar
-//   if (scrollPosition > 10) {
-//     element.classList.add("hide"); // Tambahkan kelas 'hide' saat scroll
-//     elementTwo.classList.add("hide");
-//     elementtes.classList.add("tes");
-//   } else {
-//     element.classList.remove("hide"); // Tampilkan kembali jika scroll ke atas
-//     elementTwo.classList.remove("hide");
-//     elementtes.classList.remove("tes");
-//   }
+const cl = document.querySelector(".logo-container");
+// cl.addEventListener("mouseover", function () {
+//   console.log("Mouse masuk ke elemen!");
 // });
-
+cl.addEventListener("mouseenter", function () {
+  cl.classList.add("opacity-full");
+});
+cl.addEventListener("mouseleave", function () {
+  cl.classList.remove("opacity-full");
+});
+//
 const karya = document.querySelector(".container-dua");
 window.addEventListener("scroll", () => {
   // Ambil posisi scroll saat ini
