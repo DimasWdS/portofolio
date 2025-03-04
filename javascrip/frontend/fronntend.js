81b2da853f357a7ccd4e50c744e8ad7240e5365e
dimaswidysaputra41@gamil.com <dimaswidysaputra41@gmail.com>
2025-03-04 15:47:09 +0700

    sds

--- javascrip/frontend/fronntend.js
+++ javascrip/frontend/fronntend.js
@@ -26,3 +26,17 @@ function typeEffect() {
 }
 
 window.onload = typeEffect;
+
+const htmlBtn = document.getElementById("html-btn");
+const jsBtn = document.getElementById("js-btn");
+const conHtml = document.querySelector(".konten-html");
+const conJs = document.querySelector(".konten-javascrip");
+console.log(conJs);
+jsBtn.addEventListener("click", function () {
+  conHtml.classList.add("konten-html-go");
+  conJs.classList.remove("konten-javascrip-go");
+});
+htmlBtn.addEventListener("click", function () {
+  conHtml.classList.remove("konten-html-go");
+  conJs.classList.add("konten-javascrip-go");
+});
