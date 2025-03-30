637f59d9e2a23ab1b96c29bf0e89731d1ed8c6c2
dimaswidysaputra41@gamil.com <dimaswidysaputra41@gmail.com>
2025-03-30 18:20:42 +0700

    dd

--- javascrip/nav.js
+++ javascrip/nav.js
@@ -1,20 +1,3 @@
-// Pilih elemen "contact"
-// const contactSection = document.getElementById("contack");
-// console.log(contactSection);
-// // Buat observer
-// const observer = new IntersectionObserver(
-//   (entries) => {
-//     entries.forEach((entry) => {
-//       if (entry.isIntersecting) {
-//         console.log("Masuk Contact");
-//       }
-//     });
-//   },
-//   { threshold: 0.5 }
-// ); // Akan aktif ketika 50% bagian terlihat
-
-// // Amati bagian "contact"
-// observer.observe(contactSection);
 function delay(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
