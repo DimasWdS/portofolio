c3cc6f824b84515ab0e48f94a74ed540bd9fa30c
dimaswidysaputra41@gamil.com <dimaswidysaputra41@gmail.com>
2025-04-10 17:20:53 +0700

    dd

--- javascrip/beranda.js
+++ javascrip/beranda.js
@@ -2,6 +2,6 @@ import { cloneBg, sum } from "./../module/aggregate.mjs";
 
 const oriTeks = document.querySelector(".layer-1-bg section");
 
-for (let i = 0; i <= 12; i++) {
+for (let i = 0; i <= 50; i++) {
   cloneBg(oriTeks);
 }
