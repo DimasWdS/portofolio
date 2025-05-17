88fa8136f6f4c027ead9ab32797ca8ab66e1501d
dimaswidysaputra41@gamil.com <dimaswidysaputra41@gmail.com>
2025-05-17 10:07:59 +0700

    d

--- javascrip/skills.js
+++ javascrip/skills.js
@@ -1,74 +1,63 @@
-{
-  const skills = document.getElementById("skills");
-  Object.assign(skills.style, {
-    // overflow: "hidden",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    // backgroundColor: "yellow",
-    margin: "5rem 0 5rem 0",
-  });
-  const div = document.createElement("div");
-  const div2 = document.createElement("div");
-  skills.appendChild(div);
-  skills.appendChild(div2);
-  [div, div2].forEach((el) => {
-    el.classList.add("batas");
-    Object.assign(el.style, {
-      position: "absolute",
-      width: "100%",
-      height: "1rem",
-      // backgroundColor: "blue",
-    });
-  });
-  Object.assign(div.style, {
-    top: "0",
-    transform: "rotate(2deg) scale(1.4)",
-    transformOrigin: "left",
-  });
-  Object.assign(div2.style, {
-    bottom: "0",
-    transform: "rotate(-2deg) scale(1.4)",
-    transformOrigin: "left",
-  });
-}
+const skills = document.querySelector(".skills");
+console.log(skills);
+skills.classList.add("flexCenter");
+Object.assign(skills.style, {
+  margin: "10rem 0 5rem 0",
+  width: "100%",
+  height: "100vh",
+});
 
-const conSkills = document.querySelector(".skills");
+const conSkills = document.createElement("section");
+skills.appendChild(conSkills);
+Object.assign(conSkills.style, {
+  position: "relative",
+  width: "90%",
+  maxWidth: "60rem",
+  height: "100%",
+  // backgroundColor: "blue",
+});
 
-const conGaruda = document.createElement("div");
-conSkills.appendChild(conGaruda);
-conGaruda.classList.add("conGaruda");
+const possitionGaruda = document.createElement("section");
+conSkills.appendChild(possitionGaruda);
+possitionGaruda.classList.add("possitionGaruda");
+Object.assign(possitionGaruda.style, {
+  position: "relative",
+  width: "60rem",
+  height: "20rem",
+  // backgroundColor: "cyan",
+});
+const conGaruda = document.createElement("section");
+possitionGaruda.appendChild(conGaruda);
+conGaruda.classList.add("flexCenter", "animasiBadan");
+Object.assign(conGaruda.style, {
+  position: "absolute",
+  height: "100%",
+  width: "100%",
+  // backgroundColor: "green",
+});
 // badan
 const imgBadan = document.createElement("img");
 conGaruda.appendChild(imgBadan);
 imgBadan.src = "./asset/svg/garudav2/badan.svg";
-imgBadan.style.zIndex = "2";
-imgBadan.style.scale = "1.2";
-// sayap kanan
-const imgSayapKanan = document.createElement("img");
-conGaruda.appendChild(imgSayapKanan);
-imgSayapKanan.src = "./asset/svg/garudav2/sayapKanan.svg";
-imgSayapKanan.classList.add("sayapKanan");
+imgBadan.style.zIndex = "1";
+imgBadan.style.scale = "1.7";
 // sayap kiri
 const imgSayapKiri = document.createElement("img");
 conGaruda.appendChild(imgSayapKiri);
-imgSayapKiri.classList.add("sayapKiri");
 imgSayapKiri.src = "./asset/svg/garudav2/sayapKiri.svg";
+imgSayapKiri.style.scale = "1.6";
+imgSayapKiri.classList.add("sayapKiri");
+// saap kanan
+const imgSayapKanan = document.createElement("img");
+conGaruda.appendChild(imgSayapKanan);
+imgSayapKanan.src = "./asset/svg/garudav2/sayapKanan.svg";
+imgSayapKanan.style.scale = "1.6";
+imgSayapKanan.classList.add("sayapKanan");
 
-const header = document.createElement("div");
-conSkills.appendChild(header);
-header.classList.add("conTeks");
-
-const teks1 = document.createElement("p");
-teks1.textContent = "iLLustrator";
-header.appendChild(teks1);
-teks1.classList.add("teks1");
-
-const teks3 = document.createElement("p");
-teks3.textContent = "Front End";
-header.appendChild(teks3);
-teks3.classList.add("teks2");
-
-[teks1, teks3].forEach((el) => {
-  el.classList.add("p");
+[imgBadan, imgSayapKiri, imgSayapKanan].forEach((el) => {
+  Object.assign(el.style, {
+    position: "absolute",
+    height: "100%",
+    width: "max-content",
+  });
 });
