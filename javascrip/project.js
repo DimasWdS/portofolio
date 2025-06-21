1413472d50afe805c3dea25ba6f795d62b1c43b0
dimaswidysaputra41@gamil.com <dimaswidysaputra41@gmail.com>
2025-06-21 08:58:35 +0700

    d

--- javascrip/project.js
+++ javascrip/project.js
@@ -10,7 +10,6 @@ conProject.appendChild(container);
 container.classList.add("flexCenter");
 Object.assign(container.style, {
   width: "100%",
-  maxWidth: "60rem",
   height: "100%",
   flexDirection: "column",
   // backgroundColor: "green",
@@ -18,167 +17,128 @@ Object.assign(container.style, {
 
 // iLLustrator
 {
-  const conIll = document.createElement("section");
-  container.appendChild(conIll);
-  Object.assign(conIll.style, {
-    width: "100%",
-    // backgroundColor: "blue",
-  });
+  const containerCard = document.createElement("section");
   const header = document.createElement("header");
-  const textHeader = document.createElement("p");
-  textHeader.textContent = "Recent Illustrator Projects";
-  conIll.appendChild(header);
-  header.appendChild(textHeader);
+  const hedTeks = document.createElement("p");
+
+  hedTeks.textContent = "Recent iLLustrator Projects";
+
+  containerCard.classList.add("containerCardProject");
+
+  container.appendChild(containerCard);
+  containerCard.appendChild(header);
+  header.appendChild(hedTeks);
+
   Object.assign(header.style, {
     width: "100%",
-    // height: "3rem",
-    // backgroundColor: "yellow",
   });
-  Object.assign(textHeader.style, {
-    fontWeight: "bold",
-    fontSize: "clamp(1.5rem, 2.5vw, 4rem)",
+
+  Object.assign(hedTeks.style, {
+    fontSize: "clamp(1.5rem, 5vw, 2.5rem)",
+    fontWeight: "600",
+    margin: "2rem 0 2rem 0",
     color: "var(--white)",
   });
-  const conImg = document.createElement("section");
-  conImg.classList.add("flexCenter");
-  conIll.appendChild(conImg);
-  Object.assign(conImg.style, {
-    display: "flex",
-    flexDirection: "column",
-    gap: "0.2rem",
-    width: "100%",
-    padding: "2rem 0 2rem 0",
-    // backgroundColor: "yellow",
-  });
 
-  function createContainerProject(title, pictures, deskripsi) {
-    const container = document.createElement("section");
-    container.classList.add("mobileGone", "flexCenter");
-    conImg.appendChild(container);
-    Object.assign(container.style, {
-      overflow: "hidden",
-      position: "relative",
-      width: "45rem",
-      height: "25rem",
-      backgroundColor: "var(--tes)",
-      // backgroundColor: "yellow",
-      border: "2px solid transparent",
-      borderRadius: "10px",
-      boxShadow: "rgba(0, 0, 0, 0) 1.95px 1.95px 2.6px",
-      cursor: "pointer",
+  function illustratorCard(image, judul, teksDeskripsi) {
+    function createIcon(image) {
+      const container = document.createElement("section");
+      conIcon.appendChild(container);
+
+      Object.assign(container.style, {
+        width: "3rem",
+        height: "3rem",
+        borderRadius: "50%",
+        backgroundImage: `url(./asset/icon/${image})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeate: "no-repeate",
+        outline: "0.1rem solid var(--main-font)",
+      });
+    }
+
+    const card = document.createElement("section");
+    const conImg = document.createElement("section");
+    const conDes = document.createElement("section");
+    const conImg1 = document.createElement("section");
+    const conIcon = document.createElement("section");
+    const desHeader = document.createElement("section");
+    const desHeaderContent = document.createElement("p");
+    const deskripsi = document.createElement("section");
+    const deskripsiContent = document.createElement("p");
+
+    card.classList.add("cardIllustrator");
+    conImg.classList.add("containerImg");
+    conDes.classList.add("containerDeskripsi");
+
+    desHeaderContent.textContent = `${judul}`;
+    deskripsiContent.textContent = `${teksDeskripsi}`;
+
+    containerCard.appendChild(card);
+    card.appendChild(conDes);
+    card.appendChild(conImg);
+    conImg.appendChild(conImg1);
+    conDes.appendChild(conIcon);
+    conDes.appendChild(desHeader);
+    desHeader.appendChild(desHeaderContent);
+    conDes.appendChild(deskripsi);
+    deskripsi.appendChild(deskripsiContent);
+
+    Object.assign(containerCard.style, {
+      display: "flex",
+      flexDirection: "column",
+      gap: "1rem",
     });
-    const conPictures = document.createElement("img");
-    container.appendChild(conPictures);
-    conPictures.setAttribute("src", `./asset/svg/${pictures}`);
-    // container btn
-    const conBtn = document.createElement("section");
-    container.appendChild(conBtn);
-    conBtn.classList.add("flexCenter");
-    Object.assign(conBtn.style, {
-      position: "absolute",
-      height: "2rem",
+
+    Object.assign(conIcon.style, {
+      display: "flex",
+      gap: "0.5rem",
       width: "100%",
-      // backgroundColor: "cyan",
-      bottom: "0",
-      transform: "translateY(110%)",
-    });
-    const button = document.createElement("div");
-    conBtn.appendChild(button);
-    Object.assign(button.style, {
-      height: "100%",
-      width: "10rem",
-      borderRadius: "5px 5px 0 0",
-      backgroundColor: "var(--third-color",
+      padding: "1rem",
     });
 
-    //
-    const header = document.createElement("section");
-    container.appendChild(header);
-    header.textContent = `${title}`;
-    Object.assign(header.style, {
-      position: "absolute",
-      top: "0",
-      left: "0",
-      margin: "1rem",
-      fontSize: "1.2em",
-      color: "var(--white)",
-      fontWeight: "600",
-      backgroundColor: "var(--tes)",
-      border: "0.1rem solid var(--background)",
-      padding: "0.5rem 1rem ",
-      borderRadius: "5px",
-      boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
-      transform: "translateY(-200%)",
+    createIcon("adobeillustratoricon.svg");
+
+    [desHeader, deskripsi].forEach((el) => {
+      Object.assign(el.style, {
+        width: "100%",
+      });
     });
-    // container desc
-    const conDesc = document.createElement("section");
-    container.appendChild(conDesc);
-    Object.assign(conDesc.style, {
-      position: "absolute",
-      height: "60%",
-      width: "95%",
-      bottom: "0",
-      transform: "translateY(110%)",
-      borderRadius: "15px 15px 20px 20px",
+
+    Object.assign(desHeaderContent.style, {
+      color: "var(--white)",
+      fontSize: "clamp(2em,3vh,3em)",
+      margin: "1rem 0 0 1rem",
+      fontWeight: "700",
+    });
+
+    Object.assign(deskripsiContent.style, {
+      color: "var(--main-font)",
+      fontWeight: "700",
       padding: "1rem",
-      backgroundColor: "var(--tes)",
+      fontSize: "0.8em",
+    });
+
+    Object.assign(conImg1.style, {
+      position: "absolute",
+      aspectRatio: "16/9",
+      width: "100%",
+      bottom: "0",
+      backgroundImage: `url(./asset/svg/${image})`,
       backgroundPosition: "center",
       backgroundSize: "cover",
-      border: "0.1rem solid var(--background)",
-      boxShadow:
-        "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
-    });
-    const descText = document.createElement("p");
-    conDesc.appendChild(descText);
-    descText.textContent = `${deskripsi}`;
-    Object.assign(descText.style, {
-      // backgroundColor: "yellow",
-      fontSize: "0.9em",
-      color: "var(--white)",
-      fontWeight: "500",
-    });
-    container.addEventListener("mouseenter", function () {
-      // memunculkan container btn
-      conBtn.style.transform = "translateY(0)";
-
-      // memunculkan border
-      container.style.border = "2px solid var(--third-color)";
-
-      container.style.boxShadowshadow =
-        "rgba(255, 255, 255, 0.57) 1.95px 1.95px 2.6px";
-    });
-    container.addEventListener("mouseleave", function () {
-      // menghilangkan container btn
-      conBtn.style.transform = "translateY(110%)";
-      container.style.boxShadowshadow = "rgba(0, 0, 0, 0) 1.95px 1.95px 2.6px";
-
-      // menghilangkan border
-      container.style.border = "2px solid transparent";
-      conDesc.style.transform = "translateY(110%)";
-    });
-    button.addEventListener("mouseenter", function () {
-      // memunculkan container btn
-      conDesc.style.transform = "translateY(0)";
-      header.style.transform = "translateY(0)";
-    });
-    conDesc.addEventListener("mouseleave", function () {
-      // memunculkan container btn
-      header.style.transform = "translateY(-200%)";
-      conDesc.style.transform = "translateY(110%)";
-    });
-    [container, conBtn, conDesc, header].forEach((el) => {
-      el.style.transition = "all 0.2s ease-in-out";
+      backgroundRepeate: "no-repeate",
     });
   }
-  createContainerProject(
-    "Design Livery",
+  illustratorCard(
     "mocupmobilv2.svg",
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum officiis architecto nesciunt obcaecati a enim eveniet, similique reiciendis nemo fugit facilis corrupti, provident asperiores! Minima quasi architecto repellendus incidunt nulla nesciunt recusandae fugit quos, earum sint rerum, neque, quae officia aliquam! Quisquam ad iusto nesciunt aspernatur magni soluta, corrupti quis?"
+    "Livery Design",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eveniet excepturi aspernatur error illum velit animi iusto totam corporis at officia, debitis doloremque repellat, quia nobis. Maiores quasi qui vitae?"
   );
-  createContainerProject(
-    "RRQ Badut",
+  illustratorCard(
     "mocupmobilv2.svg",
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum officiis architecto nesciunt obcaecati a enim eveniet, similique reiciendis nemo fugit facilis corrupti, provident asperiores! Minima quasi architecto repellendus incidunt nulla nesciunt recusandae fugit quos, earum sint rerum, neque, quae officia aliquam! Quisquam ad iusto nesciunt aspernatur magni soluta, corrupti quis?"
+    "Livery Design",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eveniet excepturi aspernatur error illum velit animi iusto totam corporis at officia, debitis doloremque repellat, quia nobis. Maiores quasi qui vitae?"
   );
 }
 //
@@ -285,222 +245,6 @@ Object.assign(container.style, {
   });
 }
 
-//FrontEnd
-{
-  const conFrontEnd = document.createElement("section");
-  const headerFrontEnd = document.createElement("header");
-  const headerTeks = document.createElement("p");
-  headerTeks.textContent = "Recent Website Projects";
-  container.appendChild(conFrontEnd);
-  conFrontEnd.appendChild(headerFrontEnd);
-  headerFrontEnd.appendChild(headerTeks);
-  conFrontEnd.classList.add("flexCenter");
-  Object.assign(conFrontEnd.style, {
-    // height: "100vh",
-    width: "100%",
-    flexDirection: "column",
-    gap: "0.5rem",
-  });
-  Object.assign(headerFrontEnd.style, {
-    margin: "5rem 0 2rem 0",
-    width: "100%",
-    // height: "10rem",
-    // backgroundColor: "yellow",
-  });
-  Object.assign(headerTeks.style, {
-    fontWeight: "bold",
-    fontSize: "clamp(1.5rem, 2.5vw, 4rem)",
-    color: "var(--white)",
-  });
-  //
-  function createCardWeb() {
-    const containerCard = document.createElement("section");
-    containerCard.classList.add("mobileGone");
-    containerCard.classList.add("flexCenter");
-    conFrontEnd.appendChild(containerCard);
-    Object.assign(containerCard.style, {
-      position: "relative",
-      width: "45rem",
-      height: "25rem",
-      backgroundColor: "var(--background)",
-      borderRadius: "10px",
-      // border: "0.1rem solid transparent",
-      overflow: "hidden",
-      padding: "1px",
-    });
-    const blokSpin = document.createElement("div");
-    blokSpin.classList.add("spin");
-    blokSpin.classList.add("shadowWeb");
-    containerCard.appendChild(blokSpin);
-    Object.assign(blokSpin.style, {
-      position: "absolute",
-      height: "100rem",
-      width: "10rem",
-      // backgroundColor: "green",
-      bottom: "50%",
-      // transform: "translateY(-50%)",
-      transformOrigin: "bottom",
-      // zIndex: "2",
-      background: "linear-gradient(to left, #e6521f, #80d8c3)",
-    });
-
-    const card = document.createElement("section");
-    card.classList.add("flexCenter");
-    containerCard.appendChild(card);
-    Object.assign(card.style, {
-      position: "relative",
-      // zIndex: "1",
-      height: "100%",
-      width: "100%",
-      backgroundColor: "var(--tes)",
-      borderRadius: "9px",
-      // backgroundColor: "yellow",
-    });
-
-    // deskripsi start
-    const conDesc = document.createElement("section");
-    card.appendChild(conDesc);
-    Object.assign(conDesc.style, {
-      position: "absolute",
-      display: "flex",
-      alignItems: "center",
-      height: "90%",
-      width: "50%",
-      left: "0",
-      borderRadius: "0 50% 50% 0",
-      backgroundColor: "var(--white)",
-      transform: "translateX(-200%)",
-    });
-
-    const conTeks = document.createElement("section");
-    conDesc.appendChild(conTeks);
-    Object.assign(conTeks.style, {
-      display: "flex",
-      flexDirection: "column",
-      gap: "0.5rem",
-      margin: "0 0 0 1rem",
-    });
-
-    const H1 = document.createElement("h1");
-    conTeks.appendChild(H1);
-    H1.textContent = "LOREM IPSUM";
-    Object.assign(H1.style, {
-      color: "var(--tes)",
-      fontSize: "1.5em",
-    });
-
-    const deskripsi = document.createElement("p");
-    conTeks.appendChild(deskripsi);
-    deskripsi.textContent =
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat quas ad qui nesciunt, officia deleniti veritatis sapiente necessitatibus iure sint cumque omnis corrupti libero ratione aliquam a aperiam hic quidem!";
-    Object.assign(deskripsi.style, {
-      fontSize: "0.7em",
-      color: "var(--tes)",
-      fontWeight: "500",
-    });
-
-    const tools = document.createElement("p");
-    conTeks.appendChild(tools);
-    tools.textContent = "Gergaji mesin";
-    Object.assign(tools.style, {
-      fontSize: "0.8em",
-      fontWeight: "500",
-      color: "var(--tes)",
-    });
-
-    // deskripsi end
-
-    //links start
-    const conLinks = document.createElement("section");
-    card.appendChild(conLinks);
-    conLinks.classList.add("flexCenter");
-    Object.assign(conLinks.style, {
-      position: "absolute",
-      gap: "0.2rem",
-      bottom: "0",
-      right: "0",
-      margin: "1rem",
-      transform: "translateY(200%)",
-    });
-
-    const link = document.createElement("div");
-    const link2 = document.createElement("div");
-    conLinks.appendChild(link);
-    conLinks.appendChild(link2);
-    [link, link2].forEach((el) => {
-      el.classList.add("flexCenter");
-      Object.assign(el.style, {
-        height: "3.2rem",
-        width: "3.2rem",
-        backgroundColor: "blue",
-        borderRadius: "50%",
-        backgroundColor: "var(--white)",
-        overflow: "hidden",
-      });
-    });
-    const aLink = document.createElement("a");
-    const aLink2 = document.createElement("a");
-    aLink.setAttribute("href", "#");
-    aLink2.setAttribute("href", "#");
-    link.appendChild(aLink);
-    link2.appendChild(aLink2);
-
-    [aLink, aLink2].forEach((el) => {
-      el.classList.add("flexCenter");
-      Object.assign(el.style, {
-        display: "inline-flex",
-        height: "100%",
-        width: "100%",
-        // backgroundColor: "yellow",
-      });
-    });
-
-    const linkImg = document.createElement("img");
-    const linkImg2 = document.createElement("img");
-    linkImg2.setAttribute("src", "./asset/icon/github.svg");
-    linkImg.setAttribute("src", "./asset/icon/link.svg");
-    aLink.appendChild(linkImg);
-    aLink2.appendChild(linkImg2);
-
-    [linkImg, linkImg2].forEach((el) => {
-      Object.assign(el.style, {
-        transform: "scale(0.8)",
-      });
-    });
-
-    [link, link2].forEach((el) => {
-      el.addEventListener("mouseenter", function () {
-        el.style.opacity = "70%";
-      });
-    });
-    [link, link2].forEach((el) => {
-      el.addEventListener("mouseleave", function () {
-        el.style.opacity = "100%";
-      });
-    });
-
-    //links end
-
-    [conDesc, conLinks].forEach((el) => {
-      Object.assign(el.style, {
-        transition: "all 0.5s ease-in-out",
-      });
-    });
-
-    card.addEventListener("mouseenter", function () {
-      [conDesc, conLinks].forEach((el) => {
-        el.classList.add("translate0");
-      });
-    });
-    card.addEventListener("mouseleave", function () {
-      [conDesc, conLinks].forEach((el) => {
-        el.classList.remove("translate0");
-      });
-    });
-  }
-  createCardWeb();
-  createCardWeb();
-}
 //
 {
   const conPageWeb = document.createElement("section");
