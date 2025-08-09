4253b1121449086d4187ae8f548b47ee64813a82
DimasWds <dimaswids99@gmail.com>
2025-08-09 12:14:31 +0700

    D

--- javascrip/tentang-saya.js
+++ javascrip/tentang-saya.js
@@ -2,9 +2,9 @@ const conTentangSaya = document.querySelector(".container-tentang-saya");
 Object.assign(conTentangSaya.style, {
   width: "100%",
   display: "felx",
-  width: "100%",
   // maxWidth: "80rem",
   // backgroundColor: "yellow",
+
   margin: "auto",
 });
 
@@ -72,7 +72,7 @@ Object.assign(pembungkus.style, {
     aspectRatio: "1/1",
     backgroundColor: "green",
     borderRadius: "10px",
-    backgroundImage: "url(./asset/myfoto/2.jpg",
+    backgroundImage: "url(./asset/myfoto/3.jpg",
     backgroundPosition: "center",
     backgroundSize: "cover",
     backgroundRepeate: "no-repeate",
@@ -81,66 +81,7 @@ Object.assign(pembungkus.style, {
 
 // container konten kanan
 {
-  const conKanan = document.createElement("section");
-  const container = document.createElement("section");
-  const h1 = document.createElement("h1");
-  const p = document.createElement("p");
-  const line = document.createElement("div");
-  const conBioData = document.createElement("section");
-
-  conBioData.classList.add("container-biodata");
-
-  h1.textContent = "Hello There!";
-  p.textContent =
-    "Hello, my name is Dimas Widy Saputra, and I am from Madiun, Indonesia. I have a strong interest in illustration and front-end development. With the experience and skills I possess, I am confident in my ability to create clean, modern, and functional illustration designs, as well as write efficient and well-structured code to deliver engaging and interactive interfaces";
-
-  conKanan.classList.add("bagian-kanan-aboutme");
-
-  pembungkus.appendChild(conKanan);
-  conKanan.appendChild(container);
-  container.appendChild(h1);
-  container.appendChild(p);
-  container.appendChild(line);
-  container.appendChild(conBioData);
-
-  Object.assign(conKanan.style, {
-    width: "60%",
-  });
-
-  Object.assign(container.style, {
-    margin: "auto",
-    // height: "30rem",
-    width: "90%",
-    boxShadow: "0px 3px 8px -3px rgba(0, 0, 0, 1)",
-    padding: "1rem 1.5rem 1rem 1.5rem",
-    display: "flex",
-    flexDirection: "column",
-  });
-  Object.assign(h1.style, {
-    fontSize: "1.6em",
-    color: "var(--white)",
-  });
-  Object.assign(p.style, {
-    margin: "1rem 0 0 0",
-    color: "var(--main-font)",
-    fontSize: "0.9em",
-    fontWeight: "500",
-  });
-  Object.assign(line.style, {
-    margin: "1rem 0 0 0",
-    width: "100%",
-    height: "1px",
-    backgroundColor: "var(--third-color)",
-    opacity: "60%",
-  });
-  Object.assign(conBioData.style, {
-    margin: "1.5rem 0 0 0",
-    width: "100%",
-    display: "flex",
-    justifyContent: "space-between",
-    flexWrap: "wrap",
-  });
-  function createCardBiodata(title, titleAnsw) {
+  function createCardBiodata(title, titleAnsw, icon) {
     const container = document.createElement("section");
     const conIcon = document.createElement("div");
     const conTeks = document.createElement("div");
@@ -176,6 +117,10 @@ Object.assign(pembungkus.style, {
       margin: "0 0 0 0.4rem",
       boxShadow:
         "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
+      backgroundImage: `url(./asset/icon/${icon})`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "60%",
+      backgroundPosition: "center",
     });
     Object.assign(conTeks.style, {
       // backgroundColor: "yellow",
@@ -206,10 +151,184 @@ Object.assign(pembungkus.style, {
       }
     });
   }
-  createCardBiodata("Name", "Dimas Widy Saputra");
-  createCardBiodata("Domicile", "Madiun, Jawa Timur");
-  createCardBiodata("Education", "Universitas PGRI MADIUN");
-  createCardBiodata("Email", "dimaswidysaputra41@gmail.com");
-  createCardBiodata("Phone", "+6281252966557");
-  createCardBiodata("Status", "Available for Work");
+
+  const conKanan = document.createElement("section");
+  const container = document.createElement("section");
+  const h1 = document.createElement("h1");
+  const p = document.createElement("p");
+  const line = document.createElement("div");
+  const line2 = document.createElement("div");
+  const conBioData = document.createElement("section");
+  const conUnderBiodata = document.createElement("section");
+  const header = document.createElement("header");
+  const downCV = document.createElement("button");
+  const downIcon = document.createElement("div");
+  const downTeks = document.createElement("p");
+  const hireMe = document.createElement("button");
+  const hireIcon = document.createElement("div");
+  const hireTeks = document.createElement("p");
+
+  conBioData.classList.add("container-biodata");
+
+  h1.textContent = "Hello There!";
+  p.textContent =
+    "I am a student at Universitas PGRI Madiun with a passion for illustration and website development. I enjoy learning new things and always strive to improve my skills in programming and drawing, particularly in frontend development and creating impressive illustrations.";
+  header.textContent = "Interests & Focus";
+  downTeks.textContent = "Download My CV";
+  hireTeks.textContent = "Hire Me Now";
+
+  conKanan.classList.add("bagian-kanan-aboutme");
+
+  pembungkus.appendChild(conKanan);
+  conKanan.appendChild(container);
+  container.appendChild(h1);
+  container.appendChild(p);
+  container.appendChild(line);
+  container.appendChild(conBioData);
+  container.appendChild(line2);
+  container.appendChild(conUnderBiodata);
+  conUnderBiodata.appendChild(header);
+  conUnderBiodata.appendChild(downCV);
+  downCV.appendChild(downIcon);
+  downCV.appendChild(downTeks);
+  conUnderBiodata.appendChild(hireMe);
+  hireMe.appendChild(hireIcon);
+  hireMe.appendChild(hireTeks);
+
+  Object.assign(conKanan.style, {
+    width: "60%",
+  });
+
+  Object.assign(container.style, {
+    margin: "auto",
+    // height: "30rem",
+    width: "90%",
+    boxShadow: "0px 3px 8px -3px rgba(0, 0, 0, 1)",
+    padding: "1rem 1.5rem 1rem 1.5rem",
+    display: "flex",
+    flexDirection: "column",
+  });
+  Object.assign(h1.style, {
+    fontSize: "1.6em",
+    color: "var(--white)",
+  });
+  Object.assign(p.style, {
+    margin: "1rem 0 0 0",
+    color: "var(--main-font)",
+    fontSize: "0.9em",
+    fontWeight: "500",
+  });
+  [line, line2].forEach((el) => {
+    Object.assign(el.style, {
+      margin: "1rem 0 1rem 0",
+      width: "100%",
+      height: "1px",
+      backgroundColor: "var(--third-color)",
+      opacity: "60%",
+    });
+  });
+  Object.assign(conBioData.style, {
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    flexWrap: "wrap",
+  });
+  createCardBiodata("Name", "Dimas Widy Saputra", "person.svg");
+  createCardBiodata("Domicile", "Madiun, Jawa Timur", "mappin.svg");
+  createCardBiodata("Education", "Universitas PGRI MADIUN", "school.svg");
+  createCardBiodata("Email", "dimaswidysaputra41@gmail.com", "mail.svg");
+  createCardBiodata("Phone", "+6281252966557", "phone.svg");
+  createCardBiodata("Status", "Available for Work", "work.svg");
+
+  Object.assign(conUnderBiodata.style, {
+    width: "100%",
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "0.5rem",
+    // backgroundColor: "yellow",
+  });
+  Object.assign(header.style, {
+    color: "var(--main-font)",
+    fontWeight: "600",
+    width: "100%",
+    margin: "0 0 1.5rem 0",
+    // backgroundColor: "yellow",
+  });
+  Object.assign(downCV.style, {
+    display: "inline-flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: "0.2rem",
+    backgroundColor: "var(--white)",
+    padding: "0.5em 0.2em",
+    borderRadius: "5px",
+    transition: "all 0.2s ease-in-out",
+  });
+  Object.assign(downIcon.style, {
+    height: "25px",
+    width: "25px",
+    // backgroundColor: "green",
+    backgroundImage: "url(./asset/icon/download.svg)",
+  });
+  Object.assign(downTeks.style, {
+    fontSize: "1.1em",
+    fontWeight: "800",
+    color: "var(--tes)",
+  });
+
+  Object.assign(hireMe.style, {
+    display: "inline-flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: "0.2rem",
+    backgroundColor: "var(--tes)",
+    padding: "0.5em 0.2em",
+    borderRadius: "5px",
+    transition: "all 0.2s ease-in-out",
+    border: "1px solid var(--white)",
+  });
+
+  Object.assign(hireIcon.style, {
+    height: "25px",
+    width: "25px",
+    // backgroundColor: "green",
+    backgroundImage: "url(./asset/icon/work.svg)",
+  });
+
+  Object.assign(hireTeks.style, {
+    fontSize: "1.1em",
+    fontWeight: "800",
+    color: "var(--white)",
+  });
+
+  [downCV, hireMe].forEach((el, dex) => {
+    el.addEventListener("mouseenter", function () {
+      Object.assign(el.style, {
+        transform: "translateY(-5px)",
+      });
+      if (dex === 1) {
+        Object.assign(el.style, {
+          backgroundColor: "var(--white)",
+        });
+        Object.assign(hireTeks.style, {
+          color: "var(--tes)",
+        });
+      }
+    });
+  });
+  [downCV, hireMe].forEach((el, dex) => {
+    el.addEventListener("mouseleave", function () {
+      Object.assign(el.style, {
+        transform: "translateY(0)",
+      });
+      if (dex === 1) {
+        Object.assign(el.style, {
+          backgroundColor: "var(--tes)",
+        });
+        Object.assign(hireTeks.style, {
+          color: "var(--white)",
+        });
+      }
+    });
+  });
 }
